8aa903c837e7dc2819cbd0060b805200b4c4860c
Matheus Warmeling Matias <44059946+matheussl22@users.noreply.github.com>
2023-08-25 14:52:10 -0300

    v1

---
+++ ui/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import logo from './logo.svg';
+import './App.css';
+
+function App({ lang }) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          { lang === 'BR' ? 'Aprenda React' : 'Learn React' } Real Madrid pode esperar
+        </a>
+      </header>
+    </div>
+  );
+}
+
+App.propTypes = {
+  lang: PropTypes.string.isRequired,
+};
+
+export default App;
